f40cb56a3fdb93e4c00cb9d5805df0a9647f4868
OngTK <dhdxorud@gmail.com>
2025-08-05 10:49:43 +0900

    250805_1049

--- src/main/java/comprehensive/example/boardService11_250805/controller/BoardController.java
+++ src/main/java/comprehensive/example/boardService11_250805/controller/BoardController.java
@@ -2,9 +2,9 @@ package comprehensive.example.boardService11_250805.controller;
 
 import comprehensive.example.boardService11_250805.model.dao.BoardDao;
 import comprehensive.example.boardService11_250805.model.dto.BoardDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController // 싱글톤 대체
 public class BoardController {
@@ -25,6 +25,41 @@ public class BoardController {
 
         // 3. 객체화 된 dto를 dao에게 전달후 결과를 받는다.
         boolean result = boardDao.boardWrite( boardDto );
+        // 4. 결과를 view에게 리턴한다.
+        return result;
+    }
+
+    // (2) 전체조회 기능 구현
+    @GetMapping("/board")  //☆★☆★☆★☆★
+    public ArrayList<BoardDto> boardPrint( ){
+        // - 유효성검사 ~ // - 매개변수 ~
+        // 3. dao에게 전달후 결과를 받는다.
+        ArrayList<BoardDto> result = boardDao.boardPrint( );
+        // 4. 결과를 view에게 리턴한다.
+        return result;
+    }
+
+    // (3) 삭제 기능 구현
+    @DeleteMapping("/board")  //☆★☆★☆★☆★
+    public boolean boardDelete( @RequestParam int bno ){
+        // 1.유효성검사2.객체화
+        // 3. dao에게 삭제할번호 전달후 결과를 받는다.
+        boolean result = boardDao.boardDelete( bno );
+        // 4. 결과를 view에게 리턴한다.
+        return result;
+    }
+
+    // (4) 수정 기능 구현
+    @PutMapping("/board")  //☆★☆★☆★☆★
+    public boolean boardUpdate( @RequestBody BoardDto boardDto ){
+        // 1.유효성검사
+        // 2.객체화<선택, 속성이 2개이상 이면서 하나의 dto 표현 가능하면 권장>
+        //        BoardDto boardDto = new BoardDto( bno , bcontent , null );
+        // AS-IS : 매개변수로 bcontent, bwriter을 받아 BoardDto 객체 생성
+        // TO-BE : @RequestBody 로 HTTP Body를 통해 객체 생성 단계 생략! ☆★☆★☆★
+
+        // 3. dao에게 삭제할번호 전달후 결과를 받는다.
+        boolean result = boardDao.boardUpdate( boardDto );
         // 4. 결과를 view에게 리턴한다.
         return result;
     }
--- src/main/java/comprehensive/example/boardService11_250805/model/dao/BoardDao.java
+++ src/main/java/comprehensive/example/boardService11_250805/model/dao/BoardDao.java
@@ -6,6 +6,8 @@ import comprehensive.example.boardService11_250805.model.dto.BoardDto;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class BoardDao {
     // 싱글톤 선언
@@ -47,5 +49,62 @@ public class BoardDao {
         } catch (Exception e) { System.out.println(e); } // catch end
         return false; // 예외 발생시 저장실패.
     } // func end
+
+    // (2) 전체조회 기능 구현
+    public ArrayList<BoardDto> boardPrint( ) {
+        // public : 다른패키지의 Controller 가 접근하기 위해 public 다른패키지 접근 vs private 현재클래스만
+        // ArrayList<BoardDto> : *배열* 대신에 다양한 편의성 기능을 제공하는 ArrayList 클래스
+        ArrayList<BoardDto> list = new ArrayList<>(); // 조회된 레코드(DTO) 들을 저장할 리스트 선언
+        try {
+            String sql = "select * from board"; // 1. SQL 작성
+            PreparedStatement ps = conn.prepareStatement(sql);  // 2. SQL 기재
+            // 3. SQL 매개변수 대입 , SQL문법에 ?(매개변수)가 없어서 생략
+            ResultSet rs = ps.executeQuery();  // 4. SQL 실행   , select = executeQuery()
+            // 5. SQL 결과에 따른 로직/리턴/확인
+            // 1) select 조회 결과를 레코드/행/가로단위 하나씩 조회
+            while ( rs.next() ) { // rs.next() : ResultSet 인터페이스가 갖는 (조회)결과테이블에서 다음레코드 이동 뜻
+                // 2) 현재 조회중인 레코드의 속성값 호출해서 dto 만들기
+                int bno = rs.getInt( "bno" ); // rs.get타입("가져올속성명or번호")
+                String bcontent = rs.getString("bcontent");
+                String bwriter = rs.getString("bwriter");
+                BoardDto boardDto = new BoardDto(  bno , bcontent , bwriter ); // 레코드1개(열3개) --> DTO(멤버변수3개) 1개
+                // 3) 생성된 dto를 리스트에 담아주기
+                list.add( boardDto );
+            } // while 반복문 종료
+        } catch (Exception e) { System.out.println(e); }
+        return list;
+    } // func end
+
+    // (3) 삭제 기능 구현
+    public boolean boardDelete( int bno ){ // int bno : 매개변수이면서 삭제할 게시물의 식별(pk)번호
+        try {
+            String sql = "delete from board where bno = ?";// 1. SQL 작성
+            PreparedStatement ps = conn.prepareStatement(sql);// 2. SQL 기재
+            ps.setInt(1, bno);// SQL문법내 첫번째 ? 의 INT타입으로 bno 값 대입 // 3. SQL 매개변수 대입
+            // 4. SQL 실행 , insert/update/delete 문법 실행 결과는 처리된 레코드수 반환
+            int count = ps.executeUpdate();
+            // 5. SQL 결과에 따른 로직/리턴/확인
+            if (count == 1) {  return true; } // <---sql 결과 1이면 성공
+            else {   return false; } // <--- sql 결과 1 아니면 실패
+        } catch (Exception e) {  System.out.println(e);  }
+        return false; // <--- 예외발생시 삭제실패
+    }
+
+    // (4) 수정 기능 구현
+    public boolean boardUpdate( BoardDto boardDto ){
+        try { // * try ~ catch
+            String sql = "update board set bcontent = ? where bno = ? "; // 1. SQL 작성
+            PreparedStatement ps = conn.prepareStatement(sql);// 2. SQL 기재
+            // 3. SQL 매개변수 대입 , SQL문법내 ? 개수만큼 대입
+            ps.setString( 1 , boardDto.getBcontent() ); // .setString() 사용한이유 : bcontent가 문자열 이라서
+            ps.setInt( 2 , boardDto.getBno() ); // 2 작성한이유 : SQL문법내 두번째 ? 자리 에 bno 값 대입
+            // 4. SQL 실행
+            int count = ps.executeUpdate();
+            // 5. SQL 결과에 따른 로직/리턴/확인
+            if( count == 1 ) return true; // 수정 sql 결과가 1개 이면 수정성공
+            return false; // 수정 sql 결과가 1이 아니면 수정실패
+        } catch (Exception e) { System.out.println(e);  }
+        return false; // 예외발생시 수정실패
+    }
 
 } // class end
