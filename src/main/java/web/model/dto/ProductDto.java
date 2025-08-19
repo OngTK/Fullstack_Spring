15007c7f298ce658be869f4f05b10da710334274
OngTK <dhdxorud@gmail.com>
2025-08-19 20:36:34 +0900

    250819_2036 point 관련 class 구성

--- src/main/java/web/model/dto/ProductDto.java
+++ src/main/java/web/model/dto/ProductDto.java
@@ -1,8 +1,6 @@
 package web.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
