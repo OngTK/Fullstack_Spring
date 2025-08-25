523ca8ca9188e106906adf071818744037719772
OngTK <dhdxorud@gmail.com>
2025-08-25 13:23:01 +0900

    back 게시물 출력 service, dao 완성

--- src/main/java/web/model/dao/PostDao.java
+++ src/main/java/web/model/dao/PostDao.java
@@ -1,11 +1,14 @@
 package web.model.dao;
 
 import org.springframework.stereotype.Repository;
+import web.model.dto.PageDto;
 import web.model.dto.PostDto;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class PostDao extends Dao {
@@ -32,14 +35,47 @@ public class PostDao extends Dao {
         return 0; // 실패
     } // func end
 
-    // [2] 게시물 출력
-    public PostDto findAllPost() {
-        try {
-            String sql = "select * from post";
-        } catch (Exception e) {
-            System.out.println("PostDao.printPost " + e);
+    // [2-1] 카테고리별 data의 수
+    public int getTotalCount(int cno){
+        try{
+            String sql = "select count(*) from post where cno = ? ";
+            PreparedStatement ps = conn.prepareCall(sql);
+            ps.setInt(1, cno);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) return rs.getInt(1);
+        } catch (Exception e){
+            System.out.println("PostDao.getTotalCount " + e );
         }
+        return 0;
+    } // func end
 
+    // [2-2] 조건에 해당하는 게시물 조회
+    // startRow : 페이지당 조회 시작 인덱스
+    public List<PostDto> findAll (int cno, int startRow, int count){
+        List<PostDto> postList = new ArrayList<>();
+        try{
+            String sql = "select * from post where cno = ? order by pno desc limit ?,?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1,cno);
+            ps.setInt(2,startRow);
+            ps.setInt(3,count);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                PostDto postDto = new PostDto();
+                postDto.setMno(rs.getInt("mno"));
+                postDto.setCno(rs.getInt("cno"));
+                postDto.setPcontent(rs.getString("pcontent"));
+                postDto.setPno(rs.getInt("pno"));
+                postDto.setPtitle(rs.getString("ptitle"));
+                postDto.setPdate(rs.getString("pdate"));
+                postDto.setPview(rs.getInt("pview"));
+
+                postList.add(postDto);
+            }
+
+        } catch (Exception e) {
+            System.out.println("PostDao.findAll " + e );
+        }
     } // func end
 
 } // class end
--- src/main/java/web/service/PostService.java
+++ src/main/java/web/service/PostService.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.model.dao.PostDao;
+import web.model.dto.PageDto;
 import web.model.dto.PostDto;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor // lombok, final 변수 생성자 자동 생성
@@ -18,6 +21,46 @@ public class PostService {
     // [1] 게시물 등록
     public int writePost(PostDto postDto) {
         return postDao.writePost(postDto);
+    } // func end
+
+
+    // [2] 게시물 출력
+    public PageDto findAllPost(int cno, int page, int count) {
+
+        // [2.1] 페이지 당 조회할 시작 인덱스 번호 계산
+        int startRow = (page - 1) * count;
+
+        // [2.2] 총 자료 수 확인
+        int totalCount = postDao.getTotalCount(cno);
+
+        // [2.3] 전체 페이지 수 구하기
+        int totalPage = totalCount % count == 0 ? totalCount / count : totalCount / count + 1;
+        
+        int btnCount = 5 ; // 한 화면에 보여지는 page 최대 버튼 수 ( 1~5 / 6~10 )
+        // [2.4] 시작 버튼 구하기
+        int startBtn = ( (page -1 ) / btnCount) * btnCount + 1 ;
+        
+        // [2.5] 끝 버튼 구하기
+        int endBtn = startBtn + btnCount -1 ;
+        if( endBtn > totalPage ) endBtn = totalPage; //if endBtn이 totalpage보다 크면 endBtn을 그대로 사용
+
+        // [2.6] Dao에 자료 요청
+        // 매개변수 : cno, startRow(시작 인덱스), count(페이지 당 게시물 수)
+
+        List<PostDto> postList = postDao.findAll(cno, startRow, count);
+
+        // [2.7] pageDto를 구성
+        PageDto pageDto = new PageDto();
+        pageDto.setCurrentPage( page );         // 현재 페이지 번호
+        pageDto.setTotalPage( totalPage );      // 전체 페이지 수
+        pageDto.setPerCount( count );           // 한 페이지 당 계시물 수
+        pageDto.setTotalCount(totalCount);      // 전체 게시물 수
+        pageDto.setStartBtn(startBtn);          // 페이지네이션에서 시작 페이지 번호
+        pageDto.setEndBtn(endBtn);              // 페이지네이션에서 끝 페이지 번호
+        pageDto.setData(postList);              // 실제 dto 데이터
+
+        // [2.8] 결과 반환
+        return pageDto;
     } // func end
 
 } // class end
